fa36a90747a07909ce753898d68b25f6c7442cb8
existentialnightmare <72625581+existentialnightmare@users.noreply.github.com>
2021-07-24 17:28:54 -0400

    did a ton of stuff
    Editing works

--- index.js
+++ index.js
@@ -5,6 +5,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+var blogconfig = JSON.parse(fs.readFileSync("blog-config.json"));
+
+var css = `
+body{
+  font-family: arial, helvetica, monospace;
+  font-size: 18pt;
+  color: white;
+  background: black;
+}
+.text-outer{
+  display: grid;
+  place-items: center;
+}
+`
 
 const hhead = `
 <html lang="en">
@@ -12,23 +26,69 @@ const hhead = `
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>${css}</style>
     <title>Document</title>
 </head>
 <body>
-<img src="" alt="Your icon here">`;
+<a href="/"><img src="${blogconfig.iconURL}" alt="Your icon here" class="icon" height=100></a>
+<div class="text-outer">`;
 
 const hend = `
+</div>
 </body>
 </html>`;
 
 
 app.get('/read/:id', (req, res) => {
   res.write(hhead)
-  fs.readFile('md/'+req.params.id, 'utf8', function (err,data){
+  fs.readFile('md/'+req.params.id+blogconfig.postfix, 'utf8', function (err,data){
     res.write(md.render(data));
     res.write(hend)
     res.send()
   });
+})
+
+app.get('/edit/:id', (req, res) => {
+  res.write("<html><body>")
+  fs.readFile('md/'+req.params.id+blogconfig.postfix, 'utf8', function (err,data){
+    res.write(`
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css">
+  <script src="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.js"></script>
+  <style>@import url('https://fonts.googleapis.com/css2?family=Lato&display=swap'); body{font-family: Lato;}</style>
+    <textarea id="MyID" rows="4" cols="50">${data}</textarea>
+    <input id="pw" type="password">
+    <button onclick="send()">Save</button>
+  <script>
+  var simplemde = new SimpleMDE({ element: document.getElementById("MyID") });
+  var pwf
+  var pw
+  var body
+  var bodyf
+  </script>
+  <script>
+  function send() {
+      pwf = document.getElementById("pw").value
+      pw = btoa(pwf).replace('/', '_').replace('+', '-')
+      bodyf = simplemde.value();
+      body = btoa(bodyf).replace('/', '_').replace('+', '-')
+      let path = window.location.pathname.split("/");
+      fetch('/update/'+path[2]+'/'+body+'/'+pw)
+  }
+</script>
+    `)
+    res.write(hend)
+    res.send()
+  });
+})
+
+app.get('/update/:id/:body/:auth', (req, res) => {
+  let decodedBody = Buffer.from(req.params.body.replace('_', '/').replace('-', '+'), 'base64').toString('utf-8')
+  let decodedAuth = Buffer.from(req.params.auth.replace('_', '/').replace('-', '+'), 'base64').toString('utf-8')
+  if(decodedAuth == blogconfig.password){
+    fs.writeFile('md/'+req.params.id+blogconfig.postfix, decodedBody, 'utf8', function (err,data){
+      res.send("Done")
+    })
+  }else{res.send("Auth Error")}
 })
 
 app.listen(port, () => {
---
+++ md/txt.md
@@ -0,0 +1,8 @@
+# Hello!
+This works!\
+Pretty cool right? lorem blah blah blah
+## Topic 2: Electric Bogaloo
+Whos lorem?\
+Also heres an image. Pretty cool:
+
+![Alt text](https://thisartworkdoesnotexist.com/ "a title")
